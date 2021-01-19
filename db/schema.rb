86c882fe11e399a3c73b8a16b0c8601c19b12b1e
Steven Visser <steven_v1987@live.nl>
2021-01-19 19:52:07 +0100

    small change

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_01_18_193420) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.bigint "message_id", null: false
-    t.bigint "user_id", null: false
+    t.integer "message_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["message_id"], name: "index_comments_on_message_id"
@@ -39,11 +36,6 @@ ActiveRecord::Schema.define(version: 2021_01_18_193420) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
